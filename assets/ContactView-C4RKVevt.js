659c3ba16de425848c770c57a0e68e36369b61d1
sonpham28052002 <sonpham28052002@gmail.com>
2024-08-01 10:05:15 +0700

    add dist

---
+++ assets/ContactView-C4RKVevt.js
@@ -0,0 +1,1 @@
+import{_ as c,a as t,c as a,p as o,e as s,b as _}from"./index-YB7BnoZc.js";const n={},d=e=>(o("data-v-59b1ca9e"),e=e(),s(),e),p={class:"about"},r=d(()=>_("h1",null,"This is an contact page",-1)),i=[r];function h(e,l){return t(),a("div",p,i)}const u=c(n,[["render",h],["__scopeId","data-v-59b1ca9e"]]);export{u as default};
